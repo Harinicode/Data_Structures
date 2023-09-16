51fe85b2c542139b2932022c065ec6c499b0d575
Harinicode <119438229+Harinicode@users.noreply.github.com>
2023-09-16 12:19:16 +0530

    Create Binaryrecursive.cpp

---
+++ Binaryrecursive.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int lr(int a[],int b,int end,int e,int & loc)
+{
+
+    if(b<=e)
+    {
+     int mid=int((b+e)/2);
+     if(e==a[mid])
+     {
+        loc=mid;
+        return 0;
+     }
+    else if(e<a[mid])
+      {
+          return lr(a,b,mid-1,e,loc);
+      }
+    else
+      {
+          return lr(a,mid+1,end,e,loc);
+      }
+    }
+    return 1;
+}
+int main()
+{
+  int a[15],loc,ele,f=1,beg=0,end,mid,n;
+  cout<<"Enter Size";
+  cin>>n;
+  cout<<"Enter Elements";
+  for(int i=0;i<n;i++)
+   cin>>a[i];
+   cout<<"Enter the element to be searched";
+   cin>>ele;
+   end=n-1;
+   f=lr(a,beg,end,ele,loc);
+   if(f==0)
+   cout<<"found at index: "<<loc;
+  else
+   cout<<"Not found";
+  return 0;
+
+}
